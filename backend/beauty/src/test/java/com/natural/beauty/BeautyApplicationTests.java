bc736669f7d4341c81b120b55c0ff596cac03d0e
elite <elite@localhost.localdomain>
2023-04-12 11:22:40 +0200

    init backend

---
+++ backend/beauty/src/test/java/com/natural/beauty/BeautyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natural.beauty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
